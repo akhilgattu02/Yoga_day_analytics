8416027bb92e065c613498c358df726a79a10433
Akhil Gattu <akhilgattu02@gmail.com>
2022-06-13 11:54:33 +0530

    - Removed live_graph.py - Made the single live graph to 2 self updating subplots

--- python_client_example.py
+++ python_client_example.py
@@ -5,43 +5,51 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-SOCKET = "ws://192.168.0.100:7891/yoga_mat"
-closes = np.array([])
+SOCKET = "ws://192.168.0.237:7891/"
+time_pulse = []
+pulse = []
+avg_pulse = []
+gsr = []
+time_fsr = []
+fsr = []
+
+
+
 
 #Function that executes on receiving a message
 def on_message(ws, message):
-    global closes
-    message = json.loads(message)
-    close = message["Data"]
-    closes = np.array(close)
+    global time_pulse, pulse, avg_pulse, gsr, time_fsr, fsr
+    msg_json = json.loads(message)
+    print(msg_json['sensor_type'])
+    sensor_type=msg_json['sensor_type']
+    if(sensor_type==0): # pulseox
+        time_pulse.append(msg_json["Time"])
+        pulse.append(msg_json["Pulse"])
+        avg_pulse.append(msg_json["Avg_pulse"])
+        gsr.append(msg_json["gsr"])
+
+    elif(sensor_type==1):   #fsr
+        time_fsr.append(msg_json["Time"])
+        fsr.append(msg_json["Force_FSR"])
+    
 
 #Initiate a websocket running forever
-def wsthread(closes):
+def wsthread(time_pulse, pulse, avg_pulse, gsr, time_fsr, fsr):
     ws = websocket.WebSocketApp(SOCKET, on_message=on_message)
     ws.run_forever()
 
 #Run the websocket in a background thread.
-t = threading.Thread(target=wsthread, args=(closes,))
+t = threading.Thread(target=wsthread, args=(time_pulse, pulse, avg_pulse, gsr, time_fsr, fsr,))
 t.start()
 
 #The matplotlib plots run in the main thread.
-fig = plt.figure(figsize=plt.figaspect(1))
+fig, axs = plt.subplots(2)
 
 def animate(i):
     #Render plots as a matplotlib animation
-    print(closes)
-    data_arr = closes
-
-    rows = 5
-    columns = 5
     
-    Z = data_arr.reshape(columns,rows)
-
-    ax = fig.add_subplot(1,1,1)
-
-    plt.imshow(Z,origin='lower',interpolation='bilinear', cmap='autumn',extent = [0,rows-1,0,columns-1], vmin=0, vmax = 4096)
-    plt.colorbar()
-    
+    axs[0].plot(fsr)
+    axs[1].plot(pulse)
 
 anim = FuncAnimation(
     fig, animate, interval=1000
